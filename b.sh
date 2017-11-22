8eb7f07685166f400a02c4da1b75398126498c59
bokhua <bohua.liu@jetstar.com>
2017-11-22 17:09:35 +0800

    Add auto deploy scripts

--- b.sh
+++ b.sh
@@ -4,3 +4,7 @@ dotnet ef migrations add InitialCreate
 dotnet ef database update
 dotnet build
 dotnet publish
+sudo cp -rf ./Setup/nginx_site /etc/nginx/sites-available/default
+mkdir -p /var/aspnetcore/webapi
+sudo cp -rf ./bin/Debug/netcoreapp2.0/publish/* /var/aspnetcore/webapi
+sudo cp -rf ./Setup/kestrel-mvc.service /etc/systemd/system/kestrel-mvc.service
