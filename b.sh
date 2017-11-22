2b742329b832389643d25f214db9942991c09bca
bokhua <bohua.liu@jetstar.com>
2017-11-22 17:20:01 +0800

    update bulid script

--- b.sh
+++ b.sh
@@ -1,3 +1,5 @@
+sudo service nginx stop
+sudo systemctl stop kestrel-mvc.service
 mkdir -p data
 dotnet restore
 dotnet ef migrations add InitialCreate
@@ -8,4 +10,6 @@ sudo cp -rf ./Setup/nginx_site /etc/nginx/sites-available/default
 sudo mkdir -p /var/aspnetcore/webapi
 sudo cp -rf ./bin/Debug/netcoreapp2.0/publish/* /var/aspnetcore/webapi
 sudo cp -rf ./Setup/kestrel-mvc.service /etc/systemd/system/kestrel-mvc.service
-sudo systemctl enable kestrel-mvc.service 
+sudo systemctl enable kestrel-mvc.service
+sudo systemctl start kestrel-mvc.service
+sudo service nginx start
