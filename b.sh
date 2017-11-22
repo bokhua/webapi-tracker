12480494605c69a371ce99a20577d803809e54ce
bokhua <bohua.liu@jetstar.com>
2017-11-22 17:25:10 +0800

    no message

--- b.sh
+++ b.sh
@@ -1,13 +1,16 @@
 sudo service nginx stop
 sudo systemctl stop kestrel-mvc.service
-mkdir -p data
-dotnet restore
+sudo mkdir -p /var/aspnetcore/webapi
+sudo mkdir -p data
+sudo cp -rf /var/aspnetcore/webapi/data/* ./data
+
+dotnet restore 
 dotnet ef migrations add InitialCreate
 dotnet ef database update
 dotnet build
 dotnet publish
+
 sudo cp -rf ./Setup/nginx_site /etc/nginx/sites-available/default
-sudo mkdir -p /var/aspnetcore/webapi
 sudo cp -rf ./bin/Debug/netcoreapp2.0/publish/* /var/aspnetcore/webapi
 sudo cp -rf ./Setup/kestrel-mvc.service /etc/systemd/system/kestrel-mvc.service
 sudo systemctl enable kestrel-mvc.service
