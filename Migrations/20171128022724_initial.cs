382ea0e1ad5ba7762b4bc9d4b67e45e8f02e5e07
bokhua <bohua.liu@jetstar.com>
2017-11-28 10:30:02 +0800

    add new db migrations

---
+++ Migrations/20171128022724_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace webapi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClientViews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Data = table.Column<string>(nullable: true),
+                    RemoteAddress = table.Column<string>(nullable: true),
+                    RemotePort = table.Column<string>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: false),
+                    UserAgent = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientViews", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientViews");
+        }
+    }
+}
